a5aab096acb062369b3be429ae50aaf8341ff8c2
EugeneKosiak <22067815@student.westernsydney.edu.au>
2026-04-27 12:19:35 +1000

    Pass Invalid Login sends message and check for invalid cookie

--- src/app/api/logout/cookie/route.ts
+++ src/app/api/logout/cookie/route.ts
@@ -1,14 +1,24 @@
 // TODO
+import { cookies } from "next/headers";
+import { sessionStore } from "@/app/lib/store";
 import { NextResponse } from "next/server";
 
 export async function POST() {
-  const response = NextResponse.json({ message: "Logged out successfully" });
-  response.cookies.set("session_id", "", {
-    httpOnly: true,
-    secure: true,
-    sameSite: "strict",
-    path: "/",
-    expires: new Date(0), // Expire immediately
-  });
+
+    const cookieStore = await cookies();
+    const sessionID = cookieStore.get("session_id")?.value;
+    
+    if (sessionID) {
+        sessionStore.delete(sessionID);
+    }
+    
+    const response = NextResponse.json({ message: "Logged out successfully" });
+    response.cookies.set("session_id", "", {
+        httpOnly: true,
+        secure: true,
+        sameSite: "strict",
+        path: "/",
+        expires: new Date(0), // Expire immediately
+    });
   return response;
 }
